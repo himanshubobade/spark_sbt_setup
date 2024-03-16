cf970d18bfaa6ad89e3f8b368cfafe1ffa5d1d7a
himanshu <himanshubobade007@gmail.com>
2024-03-16 10:59:05 +0530

    spark-submit change

--- src/main/scala/com/example/spark_submit.sh
+++ src/main/scala/com/example/spark_submit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-java -cp "D:/spark_setup/spark/spark-3.1.2-bin-hadoop3.2/conf/;D:/spark_setup/spark/spark-3.1.2-bin-hadoop3.2/jars/*" -Xmx1g org.apache.spark.deploy.SparkSubmit --master local[*] --class com.example.M "D:/IntelliJ projects/Assignment1/target/scala-2.12/Assignment1.jar" "C:/Users/HIMANSHUU/Desktop/LinkedInData1.txt"
+java -cp "C:/Program Setups/spark setup/spark/spark-3.1.2-bin-hadoop3.2/conf/;C:/Program Setups/spark setup/spark/spark-3.1.2-bin-hadoop3.2/jars/*" -Xmx1g org.apache.spark.deploy.SparkSubmit --master local[*] --class com.example.M "C:/Users/HIMANSHUU/IdeaProjects/spark_sbt_setup/target/scala-2.12/Assignment1.jar" "C:/Program Setups/IntelliJ Projects/sample_input_file.txt"
 
 ##spark-submit --class com.example.M --master local[*] "D:/IntelliJ projects/Assignment1/target/scala-2.12/Assignment1.jar" C:\Users\HIMANSHUU\Desktop\LinkedInData1.txt
+
+#spark-submit 2
+##spark-submit --class com.example.M --master local[*] C:\Users\HIMANSHUU\IdeaProjects\spark_sbt_setup\target\scala-2.12\Assignment1.jar sample_input_file.txt
